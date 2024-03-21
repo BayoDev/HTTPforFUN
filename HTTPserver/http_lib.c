4e96619bfe9c7b1e20735a18744d09b148b92ec1
BayoDev <giulioventuriniwork@gmail.com>
2024-03-21 21:41:45 +0100

    More comments

--- HTTPserver/http_lib.c
+++ HTTPserver/http_lib.c
@@ -1,21 +1,5 @@
 #include "http_lib.h"
-
-typedef enum{
-    OK_200,
-    BAD_REQ_400,
-    NO_AUTH_401,
-    FORBIDDEN_403,
-    NOT_FOUND_404,
-    INTERNAL_ERROR_500,
-    NOT_IMPLEMENTED_501
-} RESPONSE_CODE;
-
-typedef enum{
-    GET,
-    HEAD,
-    POST,
-    NONE        // Not implemented
-} METHOD;
+#include "http_data_structures.h"
 
 __thread FILE* DATA_STREAM;
 __thread int DATA_SOCKET; 
@@ -24,6 +8,7 @@ __thread int DATA_SOCKET;
 //      AUXILIARY
 //=====================
 
+// Basic sanity check and uniforming for file names
 char *adapt_filename(char* file){
     if(strlen(file)==0){file=malloc(sizeof(char)*2);file="/";}
 
@@ -55,181 +40,7 @@ METHOD str_to_method(char* method_str){
 
 void force_500();
 
-//=====================
-//   DATA STRUCTURES
-//=====================
 
-struct header_field{
-    char* NAME;
-    char* VALUE;
-    struct header_field* next_field;
-};
-
-struct request_data{
-    char* METHOD;
-    char* FILE;
-    char* VERSION;
-    struct header_field* other_fields; 
-};
-
-struct response_data{
-    char* CODE;
-    char* DESC;
-    char* VERSION;
-    struct header_field* other_fields;
-};
-
-struct request_data* init_request(char* METHOD,char* FILE,char* VERSION){
-    if(METHOD==NULL || FILE==NULL || VERSION==NULL) return NULL;
-    struct request_data* new = malloc(sizeof(struct request_data));
-
-    new->METHOD = malloc(sizeof(char)*(strlen(METHOD)+1));
-    strcpy(new->METHOD,METHOD);
-    new->FILE = malloc(sizeof(char)*(strlen(FILE)+1));
-    strcpy(new->FILE,FILE);
-    new->VERSION = malloc(sizeof(char)*(strlen(VERSION)+1));
-    strcpy(new->VERSION,VERSION); 
-
-    new->other_fields = NULL;
-    return new;
-}
-
-struct response_data* init_response(char* CODE,char* DESC,char* VERSION){
-    if(CODE==NULL || DESC==NULL || VERSION==NULL) return NULL;
-    struct response_data* new = malloc(sizeof(struct response_data));
-
-    new->CODE = malloc(sizeof(char)*(strlen(CODE)+1));
-    strcpy(new->CODE,CODE);
-    new->DESC = malloc(sizeof(char)*(strlen(DESC)+1));
-    strcpy(new->DESC,DESC);
-    new->VERSION = malloc(sizeof(char)*(strlen(VERSION)+1));
-    strcpy(new->VERSION,VERSION); 
-
-    new->other_fields = NULL;
-    return new;
-}
-
-void update_response(char* CODE,char* DESC,char* VERSION,struct response_data* rd_data){
-	if(CODE!=NULL){
-		rd_data->CODE = realloc(rd_data->CODE,sizeof(char)*(strlen(CODE)+1));
-		strcpy(rd_data->CODE,CODE);
-	}
-	if(DESC!=NULL){
-		rd_data->DESC = realloc(rd_data->DESC,sizeof(char)*(strlen(DESC)+1));
-		strcpy(rd_data->DESC,DESC);
-	}
-	if(VERSION!=NULL){
-		rd_data->VERSION = realloc(rd_data->VERSION,sizeof(char)*(strlen(VERSION)+1));
-		strcpy(rd_data->VERSION,VERSION);
-	}
-}
-
-void header_add_field_req(char* NAME,char* VALUE,struct request_data* st){
-    // Find placement
-    struct header_field* aux = st->other_fields;
-    while(aux!=NULL && aux->next_field!=NULL) aux=aux->next_field;
-
-    // Place struct
-    struct header_field* new = malloc(sizeof(struct header_field));
-    
-    new->NAME = malloc(sizeof(char)*(strlen(NAME)+1));
-    strcpy(new->NAME,NAME);
-
-    new->VALUE = malloc(sizeof(char)*(strlen(VALUE)+1));
-    strcpy(new->VALUE,VALUE);
-
-    new->next_field = NULL;
-
-    if(aux==NULL) st->other_fields = new;
-    else aux->next_field=new;
-    return;
-}
-
-void header_add_field_resp(char* NAME,char* VALUE,struct response_data* st){
-    // Find placement
-    struct header_field* aux = st->other_fields;
-    while(aux!=NULL && aux->next_field!=NULL) aux=aux->next_field;
-
-    // Place struct
-    struct header_field* new = malloc(sizeof(struct header_field));
-    
-    new->NAME = malloc(sizeof(char)*(strlen(NAME)+1));
-    strcpy(new->NAME,NAME);
-
-    new->VALUE = malloc(sizeof(char)*(strlen(VALUE)+1));
-    strcpy(new->VALUE,VALUE);
-
-    new->next_field = NULL;
-
-    if(aux==NULL) st->other_fields = new;
-    else aux->next_field=new;
-    return;
-}
-
-void print_request(struct request_data rq){
-    printf("%s %s %s\n",rq.METHOD,rq.FILE,rq.VERSION);
-    struct header_field* aux = rq.other_fields;
-    while(aux!=NULL){
-        printf("%s: %s\n",aux->NAME,aux->VALUE);
-        aux=aux->next_field;
-    }
-}
-
-void print_response(struct response_data rd){
-    printf("%s %s %s\n",rd.VERSION,rd.CODE,rd.DESC);
-    struct header_field* aux = rd.other_fields;
-    while(aux!=NULL){
-        printf("%s: %s\n",aux->NAME,aux->VALUE);
-        aux=aux->next_field;
-    }
-}
-
-char* resp_to_str(struct response_data rd){
-	char* ret = calloc(strlen(rd.CODE)+strlen(rd.DESC)+strlen(rd.VERSION)+5,sizeof(char));
-	sprintf(ret,"%s %s %s\r\n",rd.VERSION,rd.CODE,rd.DESC);
-	return ret;
-}
-
-char* header_to_str(struct header_field rd){
-	char* ret = calloc(strlen(rd.NAME)+strlen(rd.VALUE)+5,sizeof(char));
-	sprintf(ret,"%s: %s\r\n",rd.NAME,rd.VALUE);
-	return ret;
-}
-
-// TODO do this without recursion
-void free_header_field(struct header_field* hd){
-    if(hd==NULL || hd->next_field==NULL) return;
-    free_header_field(hd->next_field);
-    free(hd->next_field->NAME);
-    free(hd->next_field->VALUE);
-    free(hd->next_field);
-}
-
-void free_request_data(struct request_data* st){
-    if(st->other_fields!=NULL){
-        free_header_field(st->other_fields);
-        free(st->other_fields->NAME);
-        free(st->other_fields->VALUE);
-        free(st->other_fields);
-    }
-    free(st->FILE);
-    free(st->METHOD);
-    free(st->VERSION);
-    free(st);
-}
-
-void free_response_data(struct response_data* st){
-    if(st->other_fields!=NULL){
-        free_header_field(st->other_fields);
-        free(st->other_fields->NAME);
-        free(st->other_fields->VALUE);
-        free(st->other_fields);
-    }
-    free(st->CODE);
-    free(st->DESC);
-    free(st->VERSION);
-    free(st);
-}
 
 //=====================
 //     HANDLE I/O
@@ -465,6 +276,7 @@ void send_response(struct request_data* rq_data){
 //=====================
 //      INTERFACE
 //=====================
+
 
 void* handle_request(void* socket){
     DATA_SOCKET = *((int*) socket);
